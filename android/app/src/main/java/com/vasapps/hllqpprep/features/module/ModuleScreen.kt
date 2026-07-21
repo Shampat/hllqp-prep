47e7fda4945db82d53d4321c42205ba8a557770d
Shampat <sham.pat@hotmail.com>
2026-07-21 19:24:12 -0400

    Improve module selection cards

--- android/app/src/main/java/com/vasapps/hllqpprep/features/module/ModuleScreen.kt
+++ android/app/src/main/java/com/vasapps/hllqpprep/features/module/ModuleScreen.kt
@@ -6,8 +6,22 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.vasapps.hllqpprep.core.repository.ModuleRepository
+
+
+private fun moduleColor(id: String): Color {
+    return when (id) {
+        "life" -> Color(0xFFE3F2FD)
+        "accident" -> Color(0xFFE0F7FA)
+        "segregated" -> Color(0xFFF3E5F5)
+        "annuities" -> Color(0xFFFFF3E0)
+        "ethics" -> Color(0xFFFFEBEE)
+        else -> Color.White
+    }
+}
+
 
 
 @Composable
@@ -91,7 +105,9 @@ fun ModuleScreen(
                 },
 
                 colors =
-                    CardDefaults.cardColors()
+                    CardDefaults.cardColors(
+                        containerColor = moduleColor(module.id)
+                    )
 
             ) {
 
@@ -131,17 +147,6 @@ fun ModuleScreen(
 
                     )
 
-
-
-                    Text(
-
-                        text =
-                            "Start Practice →",
-
-                        style =
-                            MaterialTheme.typography.labelLarge
-
-                    )
 
 
                 }
