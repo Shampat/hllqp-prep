d72f807698592ce825ec029b2509f256b087e7c2
Shampat <sham.pat@hotmail.com>
2026-07-23 14:27:28 -0400

    Improve exam history card color

--- android/app/src/main/java/com/vasapps/hllqpprep/features/history/ExamHistoryScreen.kt
+++ android/app/src/main/java/com/vasapps/hllqpprep/features/history/ExamHistoryScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
@@ -95,7 +96,12 @@ fun ExamHistoryScreen(
             Card(
 
                 modifier =
-                    Modifier.fillMaxWidth()
+                    Modifier.fillMaxWidth(),
+
+                colors =
+                    CardDefaults.cardColors(
+                        containerColor = Color(0xFFE8F5E9)
+                    )
 
             ) {
 
