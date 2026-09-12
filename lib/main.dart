5a34c7afcbc56f435d1dafaed009bbd28f003db1
VasApps <40002381+Shampat@users.noreply.github.com>
2026-09-12 05:45:49 -0400

    Initialize unified billing service

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/quiz_provider.dart';
 import 'providers/theme_provider.dart';
+import 'services/billing_service.dart';
 import 'services/premium_service.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PremiumService.init();
+  await BillingService.instance.init();
   runApp(const HLLQPApp());
 }
 
 class HLLQPApp extends StatelessWidget {
   const HLLQPApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => QuizProvider()),
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ChangeNotifierProvider(create: (_) => PremiumService.instance),
+        ChangeNotifierProvider.value(value: PremiumService.instance),
       ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProv, _) {
@@ -28,7 +31,7 @@ class HLLQPApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             theme: themeProv.lightTheme,
             darkTheme: themeProv.darkTheme,
-            themeMode: themeProv.isDark? ThemeMode.dark : ThemeMode.light,
+            themeMode: themeProv.isDark ? ThemeMode.dark : ThemeMode.light,
             home: const HomeScreen(),
           );
         },
